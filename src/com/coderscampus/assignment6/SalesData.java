5fe7f04a70679aaf9ad51479f37936ac122e06c4
jeremyhperez <jeremyhperez@gmail.com>
2024-09-26 12:32:55 -0700

    Initial commit

---
+++ src/com/coderscampus/assignment6/SalesData.java
@@ -0,0 +1,41 @@
+package com.coderscampus.assignment6;
+
+import java.util.Objects;
+
+public class SalesData {
+	String date;
+	int unitsSold;
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public int getUnitsSold() {
+		return unitsSold;
+	}
+
+	public void setUnitsSold(int unitsSold) {
+		this.unitsSold = unitsSold;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(date, unitsSold);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SalesData other = (SalesData) obj;
+		return Objects.equals(date, other.date) && unitsSold == other.unitsSold;
+	}
+}
